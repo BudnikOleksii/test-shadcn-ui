4548b5b5e198a2580720482f026c9722c9f3169a
Oleksii Budnik <budnikoleksii@gmail.com>
2024-02-24 14:08:03 +0200

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '', // TODO document to set up this
+  base: '/test-shadcn-ui/', // TODO document to set up this
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
